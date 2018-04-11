e26671af7f30b0fb40daae8d4b532d4f9da21285
lg <575403441@qq.com>
2018-04-12 00:49:02 +0800

    * [TransitionAnimationsViewController 可以正常过渡动画了]

--- ViewController/CustomizingTheTransitionAnimations/TransitionAnimationsViewController.swift
+++ ViewController/CustomizingTheTransitionAnimations/TransitionAnimationsViewController.swift
@@ -15,13 +15,12 @@ class TransitionAnimationsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .cyan
-
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "transition", style: .plain, target: self, action: #selector(tapTransition))
     }
 
     @objc func tapTransition() {
         let vc = PresentedViewController()
-        vc.modalPresentationStyle = .custom
+//        vc.modalPresentationStyle = .custom   // 特别注意：如果只使用transitionDelegate而不使用自定义的presentation controller，千万别把这个“modalPresentationStyle”熟悉设置为“custom”，否则动画完成后屏幕一片漆黑，踩过深坑。 The presentation controller is used only when the view controller’s modalPresentationStyle property is set to UIModalPresentationCustom
         vc.transitioningDelegate = self.myTransitioningDelegate
         self.present(vc, animated: true) {
             print("\(#function) completion.")
@@ -41,6 +40,7 @@ class MyTransitioningDelegate: NSObject, UIViewControllerTransitioningDelegate {
     }
 }
 
+// Figure 10-5 shows a custom presentation and dismissal transition that animates its view diagonally. During a presentation, the presented view starts offscreen and animates diagonally up and to the left until it is visible. During a dismissal, the view reverses its direction and animates down and to the right until it is offscreen once again.
 class MyAnimator: NSObject, UIViewControllerAnimatedTransitioning {
     
     let presenting: Bool
@@ -75,7 +75,7 @@ class MyAnimator: NSObject, UIViewControllerAnimatedTransitioning {
              1. 当present时，通过“transitionContext.view(forKey: .from)”方法获取到的“fromView”为nil；
              2. 当dismiss时，通过“transitionContext.view(forKey: .to)”方法获取到的“toView”为nil。
          所以通过下面方法获取到的toView为nil时，“toView”无法添加到“containerView”上。
-         但是，不论“present”或“dismiss”情形下，通过“transitionContext.viewController(forKey: ...)都是可以获取到“fromVC”和“toVC”的，并且通过 "fromVC.view"和”toVC.view“ 也能获取到view(不为nil)，因此我觉得可以通过vc.view来获取对应view，然后再添加到containerView上。（仍有待查阅考证）
+         但是，不论“present”或“dismiss”情形下，通过“transitionContext.viewController(forKey: ...)都是可以获取到“fromVC”和“toVC”的，并且通过 "fromVC.view"和”toVC.view“ 也能获取到view(不为nil)，因此我觉得可以通过vc.view来获取对应view，然后再添加到containerView上。（虽然文档没说，但网上也看到很多这种做法，应该可行吧。 有待查证）
          
          
          //            let toView = transitionContext.view(forKey: .to)
@@ -113,10 +113,15 @@ class MyAnimator: NSObject, UIViewControllerAnimatedTransitioning {
         // And it doesn't hurt to set its start frame.
         containerView.addSubview(toView)
         
+        
+        // 在判断 "self.presenting" 时，网上有这种做法是：通过判断 “如果 fromVC.presentedViewController == toVC“ 或 ”toVC.presentingViewController == fromVC“，则是”presenting"。   这样在”MyTransitioningDelegate“中就只用创建一个”MyAnimator“对象了。
+        
         if self.presenting {
             toView.frame = toViewStartFrame
+//            containerView.insertSubview(toView, aboveSubview: fromView)   // present时默认就toView在fromView上面，可以按需交换位置
         } else {
             toView.frame = containerFrame
+            containerView.insertSubview(fromView, aboveSubview: toView)     // 需要手动把toView放到fromView下面，否则fromView在下面被挡住了看不见移出效果
         }
         
         
@@ -135,15 +140,16 @@ class MyAnimator: NSObject, UIViewControllerAnimatedTransitioning {
             
         }) { (finished) in
             
-            let success = !transitionContext.transitionWasCancelled
+            let cancelled = transitionContext.transitionWasCancelled
             
-            // After a failed presentation or successful dismissal, remove the view.
-            if (self.presenting && !success) || (!self.presenting && success) {
+            // 文档中的这句示例代码 有问题(成功dismissal时不应该移除toView)
+            // After a failed presentation or failed dismissal, remove the view.
+            if cancelled {
                 toView.removeFromSuperview()
             }
             
             // Notify UIKit that the transition has finished
-            transitionContext.completeTransition(success)
+            transitionContext.completeTransition(!cancelled)
         }
         
     }
